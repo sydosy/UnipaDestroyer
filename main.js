f5178b73ebc76e575a6afbf51cfabbdc7e3cd5e7
sydosy <sydosy@gmail.com>
2017-03-06 21:09:14 +0900

    let's destroy

---
+++ main.js
@@ -0,0 +1,36 @@
+run();
+function run(){
+    var kamoku = document.getElementsByClassName("tdKamokuList");
+    var hyouka = document.getElementsByClassName("tdHyokaList");
+    var risyuu = document.getElementsByClassName("linkMark");
+    var chromeStorage = {};
+    var masterSubjectArray = [];
+
+    chrome.storage.local.get(
+        "masterSubject",
+        function (masterSubjects) {
+            for(var i = 0; i < risyuu.length; i++) {
+                var tmp = risyuu[i].innerText.replace(/　/g,"");
+                var nbsp = String.fromCharCode( 160 );
+                tmp = tmp.replace(nbsp," ");
+                var subject = tmp.split(" ");
+                if(masterSubjects.masterSubject.indexOf(subject[1]) >= 0){
+                    risyuu[i].innerText = "Destroy";
+                }
+            }
+        }
+    );
+
+    for(var i = 0; i < hyouka.length; i++) {
+        if(!(hyouka[i].innerText === "D" || hyouka[i].innerText === "" || hyouka[i].innerText === "-")){
+            masterSubjectArray.push(kamoku[i].innerText);
+        }
+    }
+
+    if (masterSubjectArray.length != 0) {
+        chromeStorage["masterSubject"] = masterSubjectArray;
+        chrome.storage.local.set(chromeStorage, function () {
+            console.log("save!")
+        });
+    }
+}
