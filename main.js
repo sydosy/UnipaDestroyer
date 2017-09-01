f2fa760c1b8d44fe870b0c425fdd4207fd931e83
sydosy <sydosy@gmail.com>
2017-09-01 17:04:01 +0900

    refactoring

--- main.js
+++ main.js
@@ -1,52 +1,51 @@
 run();
 
 function run() {
+    //時間割画面用
     chrome.storage.local.get(
         'masterSubject',
         function (masterSubjects) {
             let timetable = $('.koma');
             timetable.each(function () {
-                let kamokuList = $(this).find('div');
-                kamokuParse(kamokuList, masterSubjects);
-                let kamokuSorted = kamokuList.sort(function (a, b) {
+                let subjectList = $(this).find('div');
+                subjectParse(subjectList, masterSubjects);
+
+                let subjectListSorted = subjectList.sort(function (a, b) {
                     return ($(a).text() < $(b).text()) ? 1 : -1;
                 });
-
+                //ソートしたのでhtmlの更新
                 $(this).children().empty();
-                $(this).children().append(kamokuSorted);
+                $(this).children().append(subjectListSorted);
 
-                rollover(kamokuList);
+                hideSubject(subjectList);
             });
 
             //1限、2限などのセルを2行分に変更
             $('.jigen').each(function () {
-                $(this).attr("rowSpan", 2);
+                $(this).attr('rowSpan', 2);
             });
             //rollover用のボタン生成
             $('.tujoHeight').after($('<tr></tr>').append($('<td></td>', {
                 colSpan: '6'
             }).append($('<button></button>', {
-                class: 'roll-button',
+                class: 'rollButton',
                 type: 'button',
                 text: '▼'
             }))));
 
-            let trigger = $('.roll-button');
-            $(trigger).on('click', function () {
-                $(this).parent().parent().prev('tr').find('.rolloverKamoku').toggle();
-            })
+            rollover();
         }
     );
 
-    //以下、履修結果取得用
-    let kamoku = $('.tdKamokuList');
-    let hyouka = $('.tdHyokaList');
+    //履修結果取得用
+    let subject = $('.tdKamokuList');
+    let gradePoint = $('.tdHyokaList');
     let chromeStorage = {};
     let masterSubjectArray = [];
 
-    for (let i = 0; i < hyouka.length; i++) {
-        if (!(hyouka[i].innerText === 'D' || hyouka[i].innerText === '' || hyouka[i].innerText === '-')) {
-            masterSubjectArray.push(kamoku[i].innerText);
+    for (let i = 0; i < gradePoint.length; i++) {
+        if (!(gradePoint[i].innerText === 'D' || gradePoint[i].innerText === '' || gradePoint[i].innerText === '-')) {
+            masterSubjectArray.push(subject[i].innerText);
         }
     }
 
@@ -58,7 +57,7 @@ function run() {
     }
 }
 
-function kamokuParse(kamokuList, masterSubjects) {
+function subjectParse(kamokuList, masterSubjects) {
     kamokuList.each(function () {
         let kamoku = $(this).attr('class') === 'linkMark' ? $(this).find('a') : $(this);
         let tmp = kamoku.text().replace(/　/g, '');
@@ -74,14 +73,22 @@ function kamokuParse(kamokuList, masterSubjects) {
     });
 }
 
-function rollover(kamokuList) {
+function hideSubject(subjectList) {
+    //表示する科目数
     const n = 5;
-    if (kamokuList.length > n) {
-        let hiddenKamoku = kamokuList.slice(n);
-        let span = hiddenKamoku.parent();
-        hiddenKamoku.remove();
+
+    if (subjectList.length > n) {
+        let hiddenSubject = subjectList.slice(n);
+        let span = hiddenSubject.parent();
+        hiddenSubject.remove();
         span.append($('<div></div>', {
-            class: 'rolloverKamoku'
-        }).append(hiddenKamoku).hide());
+            class: 'rolloverSubject'
+        }).append(hiddenSubject).hide());
     }
+}
+
+function rollover() {
+    $('.rollButton').on('click', function () {
+        $(this).parent().parent().prev('tr').find('.rolloverSubject').toggle();
+    });
 }
